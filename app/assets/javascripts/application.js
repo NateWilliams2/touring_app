39a0d4b6aca47fe1a5f69bf002d0b54d9fff4c88
NateWilliams2 <Williams2@grinnell.edu>
2019-12-06 13:10:17 -0600

    Improved styling HW13

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,13 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function navFunction() {
+    var x = document.getElementById("myLinks");
+    console.log(x)
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
